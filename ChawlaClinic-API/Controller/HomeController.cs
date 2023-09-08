4180a5db0a47abc51b6fd3612396e0177bd8c173
KhizirFarrukh <khizirfarrukh@outlook.com>
2023-09-08 22:28:30 +0500

    Initial commit

---
+++ ChawlaClinic-API/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChawlaClinic_API.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
