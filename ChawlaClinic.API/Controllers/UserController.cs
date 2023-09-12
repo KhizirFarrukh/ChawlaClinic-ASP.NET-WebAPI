e0601238649d8380514a3953184dbec75ff63beb
KhizirFarrukh <khizirkfc@gmail.com>
2023-09-12 15:22:41 +0500

    Add user API ready

--- ChawlaClinic.API/Controllers/UserController.cs
+++ ChawlaClinic.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using ChawlaClinic.API.Models;
 using ChawlaClinic.BL.DTOs.User;
+using ChawlaClinic.BL.ServiceInterfaces;
 using ChawlaClinic.Common.Commons;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,24 @@ namespace ChawlaClinic.API.Controller
     [ApiController]
     public class UserController : ControllerBase
     {
+        private IUserServiceRepo _userRepo;
+        public UserController(IUserServiceRepo userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        
         [HttpPost("AddUser")]
         public IActionResult AddUser(AddUserDTO dto)
         {
-            return Ok(new JSONResponse());
+            try
+            {
+                (bool status, string message) = _userRepo.AddUser(dto);
+                return Ok(new JSONResponse { Status = status, Message = message });
+            }
+            catch(Exception ex)
+            {
+                return Ok(new JSONResponse { Status = false, ErrorMessage = ex.Message, ErrorDescription = ex?.InnerException?.ToString() ?? string.Empty });
+            }
         }
     }
 }
