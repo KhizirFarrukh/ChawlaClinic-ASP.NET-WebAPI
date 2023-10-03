01ee239a49428afe1a6626e3806e1b4aeb00784f
KhizirFarrukh <khizirfarrukh@outlook.com>
2023-10-03 22:00:01 +0500

    Added unique constraint to payment token

--- ChawlaClinic.DAL/ApplicationDbContext.cs
+++ ChawlaClinic.DAL/ApplicationDbContext.cs
@@ -19,6 +19,14 @@ namespace ChawlaClinic.DAL
                 .HasOne(p => p.Patient)
                 .WithMany(p => p.Payments)
                 .HasForeignKey(p => p.PatientId);
+
+            modelBuilder.Entity<Payment>()
+                .HasIndex(p => p.TokenID)
+                .IsUnique();
+
+            modelBuilder.Entity<Payment>()
+                .HasIndex(p => p.SecureToken)
+                .IsUnique();
         }
 
         public DbSet<User> Users => Set<User>();
